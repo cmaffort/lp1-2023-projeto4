c9ff5bbc1f92fd8efb240e6488b2a43ffa1d7550
lucasggrama <lucasggrama>
2023-10-09 10:08:52 -0300

    correção

---
+++ code/Projeto4/dto/src/main/java/br/cefetmg/projeto4/dto/Dto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.cefetmg.projeto4.dto;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Dto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- code/dao-postgres/src/main/java/br/cefetmg/projeto4/dao/connection/ConnectionFactory.java
+++
@@ -1,9 +0,0 @@
-package br.cefetmg.projeto4.dao.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionFactory {
-
-    public Connection getConnection() throws ClassNotFoundException, SQLException;
-}
