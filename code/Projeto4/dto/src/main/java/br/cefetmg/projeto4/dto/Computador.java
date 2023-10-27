c49ba37e413936fbd047a05eb3ff8d843634f139
Lucas Guimarães Grama <lucasggrama@protonmail.com>
2023-10-27 12:51:43 -0300

    Revert "Merge pull request #13 from cmaffort/grama"
    This reverts commit dd926379bdf51e592185e1cbf11d3fb8f8f773e6, reversing
    changes made to d34d563abbd85a77986569d7a3800bb212977911.

--- code/Projeto4/dto/src/main/java/br/cefetmg/projeto4/dto/Computador.java
+++ code/Projeto4/dto/src/main/java/br/cefetmg/projeto4/dto/Computador.java
@@ -1,84 +1,74 @@
 package br.cefetmg.projeto4.dto;
 
 import java.util.Queue;
-
 public class Computador {
-    private String codigo;
-    private String doador;
+    
     private String dataDeChegada;
-    private String donatario;
-    private String marca;
-    private String modeloProcessador;
+    private final String marca;
     private int quantidadeDeRAM;
-    public Computador(String codigo, String doador, String dataDeChegada, String donatario, String marca, String modeloProcessador, int quantidadeDeRAM) {
-        this.codigo = codigo; 
-        this.doador = doador;
-        this.dataDeChegada = dataDeChegada;
-        this.donatario = donatario;
+    private int capacidadeDeArmazenamento;
+    private String modeloProcessador;
+    private String codigo;
+    private final DoadorComputadorPessoaFisica doador;
+ //   private EstadoComputador estado;
+    private Queue<String> problemas;
+    private Donatario donatario;
+    public Computador(String marca, int quantidadeDeRAM,  int capacidadeDeArmazenamento, Queue<String> problemas, DoadorComputadorPessoaFisica doador)
+    {
+   //     this.estado = EstadoComputador.NAO_RECEBIDO;
         this.marca = marca;
-        this.modeloProcessador = modeloProcessador;
         this.quantidadeDeRAM = quantidadeDeRAM;
+        this.capacidadeDeArmazenamento = capacidadeDeArmazenamento;
+        this.problemas = problemas;
+        this.doador = doador;
+        
     }
-
-    public Computador() {
-        this.marca = "Desconhecida";
-        this.doador = null;
-    }
-    // Métodos "get" para obter os valores dos campos
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public String getDoador() {
-        return doador;
-    }
-
     public String getDataDeChegada() {
         return dataDeChegada;
-    }
-
-    public String getDonatario() {
-        return donatario;
     }
 
     public String getMarca() {
         return marca;
     }
 
-    public String getModeloProcessador() {
-        return modeloProcessador;
-    }
-
     public int getQuantidadeDeRAM() {
         return quantidadeDeRAM;
     }
 
-    // Métodos "set" para definir os valores dos campos
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    public int getCapacidadeDeArmazenamento() {
+        return capacidadeDeArmazenamento;
     }
 
-    public void setDoador(String doador) {
-        this.doador = doador;
+    public String getModeloProcessador() {
+        return modeloProcessador;
     }
 
-    public void setDataDeChegada(String dataDeChegada) {
-        this.dataDeChegada = dataDeChegada;
+    public String getCodigo() {
+        return codigo;
     }
 
-    public void setDonatario(String donatario) {
+    public String getDoador() {
+        return doador.getNome();
+    }  
+
+    public void addProblemas(String problema) {
+        problemas.add(problema);
+    }
+
+    public String getDonatario() {
+        return donatario.getNome();
+    }
+
+    public void setDonatario(Donatario donatario) {
         this.donatario = donatario;
     }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
+    public void setRam(int ram)
+    {
+        this.quantidadeDeRAM = ram;
+    }
+    public void setCapacidadeDeArmazenamento(int armazenamento)
+    {
+        this.capacidadeDeArmazenamento = armazenamento;
     }
 
-    public void setModeloProcessador(String modeloProcessador) {
-        this.modeloProcessador = modeloProcessador;
-    }
-
-    public void setQuantidadeDeRAM(int quantidadeDeRAM) {
-        this.quantidadeDeRAM = quantidadeDeRAM;
-    }
 }
