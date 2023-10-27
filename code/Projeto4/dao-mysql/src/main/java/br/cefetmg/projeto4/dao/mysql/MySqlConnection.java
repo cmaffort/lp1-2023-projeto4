e10b589d6cc22e4086ec2518c7e0e5d79f0bb3eb
Lucas Guimarães Grama <lucasggrama@protonmail.com>
2023-10-27 10:27:56 -0300

    ComputadorDAO

--- code/Projeto4/dao-mysql/src/main/java/br/cefetmg/projeto4/dao/mysql/MySqlConnection.java
+++ code/Projeto4/dao-mysql/src/main/java/br/cefetmg/projeto4/dao/mysql/MySqlConnection.java
@@ -2,42 +2,22 @@ package br.cefetmg.projeto4.dao.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MySqlConnection {
-    private Connection connection;
-    private final String host;
-    private final String usuario;
-    private final String senha;
-    private final String database;
-
-    public MySqlConnection(String host, String usuario, String senha, String database) {
-        this.host = host;
-        this.usuario = usuario;
-        this.senha = senha;
-        this.database = database;
-    }
-
-    public void connect() {
-        String url = "jdbc:mysql://" + host + "/" + database;
-        try {
-            connection = DriverManager.getConnection(url, usuario, senha);
-            System.out.println("Conexão estabelecida com o banco de dados");
-        } catch (SQLException e) {
-            e.printStackTrace();
+   Connection conexao;
+    public  Connection getConexao() throws SQLException {
+                try {
+            Class.forName("com.mysql.jdbc.Driver");
+             conexao = DriverManager.getConnection("jdbc:mysql://localhost/db_projeto4", "root", "");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySqlConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch(SQLException ex)
+        {
+            System.out.println("SQLExemption:" + ex.getMessage());
+            throw new SQLException();
         }
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-    public void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+       return conexao;
     }
 }
