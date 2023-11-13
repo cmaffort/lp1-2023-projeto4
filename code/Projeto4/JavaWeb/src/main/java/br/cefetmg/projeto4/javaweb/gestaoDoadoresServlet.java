894a7c720e71eafbfddb89f800a1419baf0ea337
Lucas Guimarães Grama <108305098+Viegas13@users.noreply.github.com>
2023-11-13 09:58:33 -0300

    terminando CSU8

--- code/Projeto4/JavaWeb/src/main/java/br/cefetmg/projeto4/javaweb/gestaoDoadoresServlet.java
+++ code/Projeto4/JavaWeb/src/main/java/br/cefetmg/projeto4/javaweb/gestaoDoadoresServlet.java
@@ -54,28 +54,14 @@ public class gestaoDoadoresServlet extends HttpServlet {
             for (DoadorDTO doadores : doadoresFisicos) {
                 out.println("<tr><td>" + doadores.getNome()+ "</td><td>" + doadores.getEmail() + "</td><td>" + doadores.getComputadoresDoados()+ "</td><td>" + doadores.getCPF() + "</td><td>" + doadores.getCodigo());
             }
-
-            
-
-            // Escrever a página HTML
-            out.println("<html><head>");
-            out.println("<link rel=\"stylesheet\" href=\"style/header.css\">");
-            out.println("<link rel=\"stylesheet\" href=\"style/main.css\">");
-            out.println("<link rel=\"stylesheet\" href=\"style/gestaoDoadores.css\">");
-            out.println("<link rel=\"stylesheet\" href=\"style/table.css\">");
-            out.println("</head><body>");
-            out.println("<header></header>");
-            out.println("<main><section>");
-            out.println("<table>");
-            out.println("<caption>Lista de Doadores Juridicos</caption>");
-            out.println("<thead>");
-            out.println("<tr><th>Nome</th><th>Email</th><th>Computadores doados</th><th>CPF</th><th>Codigo</th></tr>");
-            out.println("</thead><tbody>");
-
-            // Iterar sobre os resultados e exibi-los na tabela HTML
             for (DoadorJuridicoDTO doadores : doadoresJuridicos) {
                 out.println("<tr><td>" + doadores.getNome()+ "</td><td>" + doadores.getEmail()+ "</td><td>" + doadores.getComputadoresDoados() + "</td><td>" + doadores.getCNPJ() + "</td><td>" + doadores.getCodigo());
             }
+
+            out.println("</table></body></html>");
+
+
+            // Iterar sobre os resultados e exibi-los na tabela HTML
         }
         catch (SQLException ex) {
             Logger.getLogger(GestaoDoacao.class.getName()).log(Level.SEVERE, null, ex);
